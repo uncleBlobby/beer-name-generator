f5c3ac3467f81808a42a0ef0f642c9111797df28
Dustin <dc.christianson@gmail.com>
2022-11-05 11:05:29 -0600

    added logo, working generator button chooses random adjective and noun and returns new beer name

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import words from 'categorized-words'
+import logo from './logo.png';
 import './App.css'
 
+const nouns = words.N;
+const adjectives = words.A;
+const verbs = words.V;
+
+const pronouns = words.P;
+
+const interjections = words.I;
+
 function App() {
-  const [count, setCount] = useState(0)
 
   const [beerName, setBeerName] = useState('');
 
   const generateBeerName = () => {
-    const beerName = "Hello, beer!";
+    const beerName = getRandomAdjective() + " " + getRandomNoun();
     setBeerName(beerName);
     console.log(`New beer name: ${beerName}`)
+    //console.log(words);
+    //console.log(getRandomNoun())
+  }
+
+  const getRandomAdjective = () => {
+    const adjective = adjectives[Math.floor(Math.random()*adjectives.length)]
+    return adjective;
+  }
+
+  const getRandomNoun = () => {
+    const noun = nouns[Math.floor(Math.random()*nouns.length)]
+    //console.log(noun);
+    return noun;
   }
 
   return (
     <div className="App">
-      Malty National Beer Name Generator
+      <img src={logo} />
+      <div className="title-header">
+        <div className="sub-title">
+          Beer Name Generator
+        </div>
+        
+      </div>
+      
       <div className='beerNameDisplay'>
         {beerName}
       </div>
